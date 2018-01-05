b8eaaf633f15a81266783d121e48116deac4eaa8
陈宗强 <781611293@qq.com>
2018-01-05 22:30:39 +0800

    ***

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
   function PreLoad(imgs, options) {
     this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
     this.opts = $.extend({}, PreLoad.DEFAULTS, options);
@@ -14,17 +14,17 @@
     each: null, //每张图片加载完毕后执行
     all: null //所有图片加载完后执行
   };
-  PreLoad.prototype._unordered = function(){   //无序加载
+  PreLoad.prototype._unordered = function () {   //无序加载
     var imgs = this.imgs,
-        opts = this.opts,
-        count = 0,
-        len = imgs.length;
-    $.each(imgs, function(i, src) {
+      opts = this.opts,
+      count = 0,
+      len = imgs.length;
+    $.each(imgs, function (i, src) {
       if (typeof src != 'string') {
         return;
       }
       var imgObj = new Image();
-      $(imgObj).on('load error', function() {
+      $(imgObj).on('load error', function () {
         opts.each && opts.each(count);
         if (count >= len - 1) {
           opts.all && opts.all();
@@ -34,31 +34,31 @@
       imgObj.src = src;
     });
   };
-  PreLoad.prototype._ordered = function() {
+  PreLoad.prototype._ordered = function () {
     var opts = this.opts,
-        imgs = this.imgs,
-        len = imgs.length,
-        count = 0;
+      imgs = this.imgs,
+      len = imgs.length,
+      count = 0;
 
-        load();
+    load();
 
-        function load() {
-          var imgObj = new Image();
-          $(imgObj).on('load error', function() {
-              opts.each && opts.each(count);
-              if (count >= len) {
-                //所有图片已经加载完毕
-                opts.all && opts.all();
-              } else {
-                load();
-              }
-              count++;
-          });
-          imgObj.src = imgs[count];
+    function load() {
+      var imgObj = new Image();
+      $(imgObj).on('load error', function () {
+        opts.each && opts.each(count);
+        if (count >= len) {
+          //所有图片已经加载完毕
+          opts.all && opts.all();
+        } else {
+          load();
         }
+        count++;
+      });
+      imgObj.src = imgs[count];
+    }
   }
   $.extend({
-    preload: function(imgs, opts) {
+    preload: function (imgs, opts) {
       new PreLoad(imgs, opts);
     }
   });
